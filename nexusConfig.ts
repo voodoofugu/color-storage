0787f0252df8de79cf622c3f626a401c85d161f9
voodoofug <voodoofugu@gmail.com>
2025-10-09 18:43:06 +0300

    nexus fix

--- nexusConfig.ts
+++ nexusConfig.ts
@@ -20,214 +20,207 @@ const { state, actions } = createReactStore({
     themeSettings: "system",
   } as MyStateT,
 
-  actions: (set) => {
-    const self = {
-      setStateWithTimeout: <K extends keyof MyStateT>(
-        stateKey: K,
-        temporaryValue: MyStateT[K],
-        duration: number
-      ) => {
-        set((prev) => {
-          const hasTimeout = stateKey in prev.timeouts;
+  actions: (set) => ({
+    setStateWithTimeout: <K extends keyof MyStateT>(
+      stateKey: K,
+      temporaryValue: MyStateT[K],
+      duration: number
+    ) => {
+      set((prev) => {
+        const hasTimeout = stateKey in prev.timeouts;
 
-          // если уже есть таймер, значит originalValue уже сохранён — просто обновим таймер
-          const originalValue = hasTimeout
-            ? prev.timeouts[stateKey].originalValue
-            : prev[stateKey];
+        // если уже есть таймер, значит originalValue уже сохранён — просто обновим таймер
+        const originalValue = hasTimeout
+          ? prev.timeouts[stateKey].originalValue
+          : prev[stateKey];
 
-          // если таймер уже есть, очистим его
-          const prevTimeout = hasTimeout ? prev.timeouts[stateKey].id : null;
-          if (prevTimeout) clearTimeout(prevTimeout);
+        // если таймер уже есть, очистим его
+        const prevTimeout = hasTimeout ? prev.timeouts[stateKey].id : null;
+        if (prevTimeout) clearTimeout(prevTimeout);
 
-          const timeoutId = setTimeout(() => {
-            set((current) => {
-              const { [stateKey]: _, ...newTimeouts } = current.timeouts;
+        const timeoutId = setTimeout(() => {
+          set((current) => {
+            const { [stateKey]: _, ...newTimeouts } = current.timeouts;
 
-              return {
-                [stateKey]: originalValue,
-                timeouts: newTimeouts,
-              };
-            });
-          }, duration);
-
-          return {
-            [stateKey]: temporaryValue,
-            timeouts: {
-              ...prev.timeouts,
-              [stateKey]: {
-                id: timeoutId,
-                originalValue,
-              },
-            },
-          };
-        });
-      },
-
-      setMainColor: (color: string) => {
-        set({ mainColor: color });
-      },
-
-      setActiveColor: (color: string) => {
-        set({ activeColor: color });
-      },
-
-      setNewPalette: (paletteName?: string) => {
-        set((prev) => {
-          const existingNames = prev.colorStorage.map(
-            (item) => Object.keys(item)[0]
-          );
-
-          const getNextAvailableName = (): string => {
-            let index = prev.colorStorage.length + 1;
-            while (existingNames.includes(`Palette-${index}`)) {
-              index++;
-            }
-            return `Palette-${index}`;
-          };
-
-          const uniqueName =
-            paletteName && !existingNames.includes(paletteName)
-              ? paletteName
-              : getNextAvailableName();
-
-          return {
-            colorStorage: [...prev.colorStorage, { [uniqueName]: [] }],
-          };
-        });
-      },
-
-      setCurrentPaletteId: (id?: number) => {
-        set((prev) => ({
-          currentPaletteId:
-            typeof id === "number" ? id : prev.colorStorage.length - 1,
-        }));
-      },
-
-      setNewPaletteColor: (color: string) => {
-        let isDuplicate = false;
-
-        set((prev) => {
-          isDuplicate = prev.colorStorage.some((palette) => {
-            const [name, colors] = Object.entries(palette)[0];
-            return (
-              name ===
-                Object.keys(prev.colorStorage[prev.currentPaletteId])[0] &&
-              colors.includes(color)
-            );
-          });
-
-          if (isDuplicate) return prev;
-
-          return {
-            colorStorage: prev.colorStorage.map((palette, index) => {
-              const [name, colors] = Object.entries(palette)[0];
-              return index === prev.currentPaletteId
-                ? { [name]: [color, ...colors] }
-                : palette;
-            }),
-          };
-        });
-
-        if (isDuplicate) {
-          self.setStateWithTimeout("failedColorAdding", true, 600);
-        }
-      },
-
-      paletteRename: (newName: string) => {
-        set((prev) => ({
-          colorStorage: prev.colorStorage.map((palette) => {
-            const [name, colors] = Object.entries(palette)[0];
-            return name ===
-              Object.keys(prev.colorStorage[prev.currentPaletteId])[0]
-              ? { [newName]: colors }
-              : palette;
-          }),
-        }));
-      },
-
-      deleteCurrentPalette: (currentPaletteId: number) => {
-        set((prev) => ({
-          colorStorage: prev.colorStorage.filter(
-            (_, index) => index !== currentPaletteId
-          ),
-          currentPaletteId:
-            prev.currentPaletteId === 0 ? 0 : prev.currentPaletteId - 1,
-        }));
-      },
-
-      setNewColorsOrder: (newOrder: string[]) => {
-        set((prev) => ({
-          colorStorage: prev.colorStorage.map((palette) => {
-            const [name, _] = Object.entries(palette)[0];
-            return name ===
-              Object.keys(prev.colorStorage[prev.currentPaletteId])[0]
-              ? { [name]: newOrder }
-              : palette;
-          }),
-        }));
-      },
-
-      clearColor: (color: string) => {
-        set((prev) => ({
-          colorStorage: prev.colorStorage.map((palette) => {
-            const [name, colors] = Object.entries(palette)[0];
             return {
-              [name]: colors.filter((c) => c !== color),
+              [stateKey]: originalValue,
+              timeouts: newTimeouts,
             };
-          }),
-        }));
-      },
+          });
+        }, duration);
 
-      popupOpen: (
-        content: PopupContentT | null,
-        props?: { [key: string]: unknown }
-      ) => {
-        const currentPopup = state.getNexus("popupContent");
-        if (currentPopup) {
-          self.popupClose();
-
-          setManagedTask(
-            () => {
-              self.popupOpen(content, props);
+        return {
+          [stateKey]: temporaryValue,
+          timeouts: {
+            ...prev.timeouts,
+            [stateKey]: {
+              id: timeoutId,
+              originalValue,
             },
-            200,
-            "popupReopen"
+          },
+        };
+      });
+    },
+
+    setMainColor: (color: string) => {
+      set({ mainColor: color });
+    },
+
+    setActiveColor: (color: string) => {
+      set({ activeColor: color });
+    },
+
+    setNewPalette: (paletteName?: string) => {
+      set((prev) => {
+        const existingNames = prev.colorStorage.map(
+          (item) => Object.keys(item)[0]
+        );
+
+        const getNextAvailableName = (): string => {
+          let index = prev.colorStorage.length + 1;
+          while (existingNames.includes(`Palette-${index}`)) {
+            index++;
+          }
+          return `Palette-${index}`;
+        };
+
+        const uniqueName =
+          paletteName && !existingNames.includes(paletteName)
+            ? paletteName
+            : getNextAvailableName();
+
+        return {
+          colorStorage: [...prev.colorStorage, { [uniqueName]: [] }],
+        };
+      });
+    },
+
+    setCurrentPaletteId: (id?: number) => {
+      set((prev) => ({
+        currentPaletteId:
+          typeof id === "number" ? id : prev.colorStorage.length - 1,
+      }));
+    },
+
+    setNewPaletteColor(color: string) {
+      let isDuplicate = false;
+
+      set((prev) => {
+        isDuplicate = prev.colorStorage.some((palette) => {
+          const [name, colors] = Object.entries(palette)[0];
+          return (
+            name === Object.keys(prev.colorStorage[prev.currentPaletteId])[0] &&
+            colors.includes(color)
           );
-          return;
-        }
+        });
 
-        set({ popupContent: props ? { content, props } : content });
+        if (isDuplicate) return prev;
 
-        const popupEl = document.querySelector(
-          `.popup-window`
-        ) as HTMLDivElement;
-        popupEl!.classList.add("opening");
+        return {
+          colorStorage: prev.colorStorage.map((palette, index) => {
+            const [name, colors] = Object.entries(palette)[0];
+            return index === prev.currentPaletteId
+              ? { [name]: [color, ...colors] }
+              : palette;
+          }),
+        };
+      });
 
-        setTimeout(() => {
-          popupEl!.classList.remove("opening");
-        }, 200);
-      },
+      if (isDuplicate) {
+        this.setStateWithTimeout("failedColorAdding", true, 600);
+      }
+    },
 
-      popupClose: () => {
-        const popup = document.querySelector(`.popup-window`)!;
+    paletteRename: (newName: string) => {
+      set((prev) => ({
+        colorStorage: prev.colorStorage.map((palette) => {
+          const [name, colors] = Object.entries(palette)[0];
+          return name ===
+            Object.keys(prev.colorStorage[prev.currentPaletteId])[0]
+            ? { [newName]: colors }
+            : palette;
+        }),
+      }));
+    },
 
-        if (popup) {
-          popup.classList.add("closing");
-        }
+    deleteCurrentPalette: (currentPaletteId: number) => {
+      set((prev) => ({
+        colorStorage: prev.colorStorage.filter(
+          (_, index) => index !== currentPaletteId
+        ),
+        currentPaletteId:
+          prev.currentPaletteId === 0 ? 0 : prev.currentPaletteId - 1,
+      }));
+    },
+
+    setNewColorsOrder: (newOrder: string[]) => {
+      set((prev) => ({
+        colorStorage: prev.colorStorage.map((palette) => {
+          const [name, _] = Object.entries(palette)[0];
+          return name ===
+            Object.keys(prev.colorStorage[prev.currentPaletteId])[0]
+            ? { [name]: newOrder }
+            : palette;
+        }),
+      }));
+    },
+
+    clearColor: (color: string) => {
+      set((prev) => ({
+        colorStorage: prev.colorStorage.map((palette) => {
+          const [name, colors] = Object.entries(palette)[0];
+          return {
+            [name]: colors.filter((c) => c !== color),
+          };
+        }),
+      }));
+    },
+
+    popupOpen(
+      content: PopupContentT | null,
+      props?: { [key: string]: unknown }
+    ) {
+      const currentPopup = state.getNexus("popupContent");
+      if (currentPopup) {
+        this.popupClose();
 
         setManagedTask(
           () => {
-            set({ popupContent: null });
-            popup!.classList.remove("closing");
+            this.popupOpen(content, props);
           },
           200,
-          "popupClose"
+          "popupReopen"
         );
-      },
-    };
+        return;
+      }
 
-    return self;
-  },
+      set({ popupContent: props ? { content, props } : content });
+
+      const popupEl = document.querySelector(`.popup-window`) as HTMLDivElement;
+      popupEl!.classList.add("opening");
+
+      setTimeout(() => {
+        popupEl!.classList.remove("opening");
+      }, 200);
+    },
+
+    popupClose: () => {
+      const popup = document.querySelector(`.popup-window`)!;
+
+      if (popup) {
+        popup.classList.add("closing");
+      }
+
+      setManagedTask(
+        () => {
+          set({ popupContent: null });
+          popup!.classList.remove("closing");
+        },
+        200,
+        "popupClose"
+      );
+    },
+  }),
 });
 
 export { state, actions };
